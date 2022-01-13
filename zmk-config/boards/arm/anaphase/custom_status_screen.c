8f58ead400f6c0a02129fdc93b85cd2aa858b5d8
reversebias <cam@reversebias.net>
2022-01-13 18:35:53 +1100

    mutex problem with battery level

--- zmk-config/boards/arm/anaphase/custom_status_screen.c
+++ zmk-config/boards/arm/anaphase/custom_status_screen.c
@@ -1,8 +1,14 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+//#include <zmk/display/widgets/output_status.h>
 #include "widgets/battery_status.h"
-#include "widgets/output_status.h"
-#include "widgets/layer_status.h"
-#include "widgets/wpm_status.h"
-#include "custom_status_screen.h"
+//#include <zmk/display/widgets/layer_status.h>
+//#include <zmk/display/widgets/wpm_status.h>
+#include <zmk/display/status_screen.h>
 
 #include <logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
@@ -11,62 +17,53 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 static struct zmk_widget_battery_status battery_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
 static struct zmk_widget_output_status output_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
 static struct zmk_widget_layer_status layer_status_widget;
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_WPM_STATUS)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
 static struct zmk_widget_wpm_status wpm_status_widget;
 #endif
 
+lv_style_t global_style;
+
 lv_obj_t *zmk_display_status_screen() {
-
     lv_obj_t *screen;
-    screen = lv_obj_create(NULL, NULL);
 
+    lv_style_init(&global_style);
+    lv_style_set_text_font(&global_style, LV_STATE_DEFAULT, &lv_font_unscii_8);
+    lv_style_set_text_letter_space(&global_style, LV_STATE_DEFAULT, 1);
+    lv_style_set_text_line_space(&global_style, LV_STATE_DEFAULT, 1);
+
+    screen = lv_obj_create(NULL, NULL);
+    //lv_obj_add_style(screen, LV_LABEL_PART_MAIN, &global_style);
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
-    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_MID,
-                 0, 2);
+    lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT,
+                 0, 0);
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_OUTPUT_STATUS)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_MID, 0,
-                 41);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), NULL, LV_ALIGN_IN_TOP_LEFT, 0,
+                 0);
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_LAYER_STATUS)
-    //lv_style_set_pad_inner(&layerstyle, LV_STATE_DEFAULT, 12);
-    //lv_obj_add_style(&layer_status_widget, LV_WIDGET_PART_MAIN, &layerstyle);
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_LAYER_STATUS)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0,
-                 -5);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT,
+                 0, 0);
 #endif
 
-#if IS_ENABLED(CONFIG_CUSTOM_WIDGET_WPM_STATUS)
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_MID, 0,
-                 -5);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, -12,
+                 0);
 #endif
-
-#if CONFIG_BOARD_ANAPHASE_LEFT
-
-#endif
-
-#if CONFIG_BOARD_ANAPHASE_RIGHT
-
-#endif
-
-
-    //lv_task_handler();
-    lv_refr_now(NULL);
-    //display_blanking_off(display_dev);
-
     return screen;
 }
--- zmk-config/boards/arm/anaphase/widgets/battery_status.c
+++ zmk-config/boards/arm/anaphase/widgets/battery_status.c
@@ -11,7 +11,8 @@
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/display.h>
-#include <zmk/display/widgets/battery_status.h>
+#include "battery_status.h"
+#include <src/lv_themes/lv_theme.h>
 #include <zmk/usb.h>
 #include <zmk/events/usb_conn_state_changed.h>
 #include <zmk/event_manager.h>
@@ -21,34 +22,21 @@ static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
 struct battery_status_state {
     uint8_t level;
-#if IS_ENABLED(CONFIG_USB)
     bool usb_present;
-#endif
 };
 
 static void set_battery_symbol(lv_obj_t *label, struct battery_status_state state) {
-    char text[2] = "  ";
+    char text[8] = "";
 
     uint8_t level = state.level;
 
-#if IS_ENABLED(CONFIG_USB)
     if (state.usb_present) {
-        strcpy(text, LV_SYMBOL_CHARGE);
-    }
-#endif /* IS_ENABLED(CONFIG_USB) */
-
-    if (level > 95) {
-        strcat(text, LV_SYMBOL_BATTERY_FULL);
-    } else if (level > 65) {
-        strcat(text, LV_SYMBOL_BATTERY_3);
-    } else if (level > 35) {
-        strcat(text, LV_SYMBOL_BATTERY_2);
-    } else if (level > 5) {
-        strcat(text, LV_SYMBOL_BATTERY_1);
+        sprintf(text, "CHG:%3d%%", level);
     } else {
-        strcat(text, LV_SYMBOL_BATTERY_EMPTY);
+        sprintf(text, "Bat:%3d%%", level);
     }
-    //lv_label_set_text(label, text);
+    
+    lv_label_set_text(label, text);
 }
 
 void battery_status_update_cb(struct battery_status_state state) {
@@ -59,9 +47,7 @@ void battery_status_update_cb(struct battery_status_state state) {
 static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
     return (struct battery_status_state) {
         .level = bt_bas_get_battery_level(),
-#if IS_ENABLED(CONFIG_USB)
         .usb_present = zmk_usb_is_powered(),
-#endif /* IS_ENABLED(CONFIG_USB) */
     };
 }
 
@@ -69,12 +55,10 @@ ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state,
                             battery_status_update_cb, battery_status_get_state)
 
 ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
-#if IS_ENABLED(CONFIG_USB)
 ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
-#endif /* IS_ENABLED(CONFIG_USB) */
 
 int zmk_widget_battery_status_init(struct zmk_widget_battery_status *widget, lv_obj_t *parent) {
-    //widget->obj = lv_img_create(parent, NULL);
+    widget->obj = lv_label_create(parent, NULL);
 
     lv_obj_set_size(widget->obj, 40, 15);
 
