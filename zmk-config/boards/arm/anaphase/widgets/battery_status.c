0ee94aee3af4d2c55206b27a90e1a0f6bb92d1a6
reversebias <cam@reversebias.net>
2022-01-16 15:07:28 +1100

    updated to stock listener macro

--- zmk-config/boards/arm/anaphase/widgets/battery_status.c
+++ zmk-config/boards/arm/anaphase/widgets/battery_status.c
@@ -20,75 +20,54 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-K_MUTEX_DEFINE(battery_status_mutex);
-
 struct battery_status_state {
     uint8_t level;
     bool usb_present;
-} battery_status_state;
+};
 
-void set_battery_symbol(lv_obj_t *label) {
+static void set_battery_symbol(lv_obj_t *label, struct battery_status_state state) {
+    char text[10] = "          ";
 
-    k_mutex_lock(&battery_status_mutex, K_FOREVER);
+    uint8_t level = state.level;
 
-    char text[10] = "  ";
-
-    uint8_t level = battery_status_state.level;
-
-    if (battery_status_state.usb_present) {
+    if (state.usb_present) {
         sprintf(text, "CHG:%3d%%", level);
     } else {
         sprintf(text, "BAT:%3d%%", level);
     }
     
     lv_label_set_text(label, text);
-
-    k_mutex_unlock(&battery_status_mutex);
 }
 
-void battery_status_update_cb(struct k_work *work) {
+void battery_status_update_cb(struct battery_status_state state) {
     struct zmk_widget_battery_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_battery_symbol(widget->obj); }
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_battery_symbol(widget->obj, state); }
 }
 
-struct battery_status_state battery_status_get_state() {
-    k_mutex_lock(&battery_status_mutex, K_FOREVER);
-    
-    battery_status_state.level = bt_bas_get_battery_level();
-    battery_status_state.usb_present = zmk_usb_is_powered();
-
-    k_mutex_unlock(&battery_status_mutex);
-
-    return battery_status_state;
+static struct battery_status_state battery_status_get_state(const zmk_event_t *eh) {
+    return (struct battery_status_state) {
+        .level = bt_bas_get_battery_level(),
+        .usb_present = zmk_usb_is_powered(),
+    };
 }
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_battery_status, struct battery_status_state,
+                            battery_status_update_cb, battery_status_get_state)
+
+ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
+ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
 
 int zmk_widget_battery_status_init(struct zmk_widget_battery_status *widget, lv_obj_t *parent) {
     widget->obj = lv_label_create(parent, NULL);
 
     lv_obj_set_size(widget->obj, 64, 10);
-    
-    battery_status_get_state();
-    set_battery_symbol(widget->obj);
+
     sys_slist_append(&widgets, &widget->node);
 
+    widget_battery_status_init();
     return 0;
 }
 
 lv_obj_t *zmk_widget_battery_status_obj(struct zmk_widget_battery_status *widget) {
     return widget->obj;
 }
-
-K_WORK_DEFINE(battery_status_update_work, battery_status_update_cb);
-
-int battery_status_listener(const zmk_event_t *eh) {
-    if (zmk_display_is_initialized()) { 
-        battery_status_get_state();
-
-        k_work_submit_to_queue(zmk_display_work_q(), &battery_status_update_work);
-    }
-    return 0;
-}
-
-ZMK_LISTENER(widget_battery_status, battery_status_listener);
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_battery_state_changed);
-ZMK_SUBSCRIPTION(widget_battery_status, zmk_usb_conn_state_changed);
--- zmk-config/boards/arm/anaphase/widgets/layer_status.c
+++ zmk-config/boards/arm/anaphase/widgets/layer_status.c
@@ -5,8 +5,6 @@
  */
 
 #include <kernel.h>
-#include <bluetooth/services/bas.h>
-
 #include <logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -19,72 +17,53 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-K_MUTEX_DEFINE(layer_status_mutex);
-
 struct layer_status_state {
     uint8_t index;
     const char *label;
-} layer_status_state;
+};
 
-void set_layer_symbol(lv_obj_t *label) {
-
-    k_mutex_lock(&layer_status_mutex, K_FOREVER);
-
-    if (layer_status_state.label == NULL) {
-        char text[6] = {};
+static void set_layer_symbol(lv_obj_t *label, struct layer_status_state state) {
+    if (state.label == NULL) {
+        char text[10] = "          ";
 
         sprintf(text, "BASE");
 
         lv_label_set_text(label, text);
     } else {
-        char text[12] = {};
+        char text[12] = "          ";
 
-        snprintf(text, 12, "%s", layer_status_state.label);
+        snprintf(text, 12, "%s", state.label);
 
         lv_label_set_text(label, text);
     }
-
-    k_mutex_unlock(&layer_status_mutex);
 }
 
-void layer_status_update_cb(struct k_work *work) {
+static void layer_status_update_cb(struct layer_status_state state) {
     struct zmk_widget_layer_status *widget;
-    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_layer_symbol(widget->obj); }
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_layer_symbol(widget->obj, state); }
 }
 
-void layer_status_get_state() {
-    k_mutex_lock(&layer_status_mutex, K_FOREVER);
-
+static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
     uint8_t index = zmk_keymap_highest_layer_active();
-    layer_status_state.index = index;
-    layer_status_state.label = zmk_keymap_layer_label(index);
-
-    k_mutex_unlock(&layer_status_mutex);
+    return (struct layer_status_state){.index = index, .label = zmk_keymap_layer_label(index)};
 }
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
+                            layer_status_get_state)
+
+ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
 
 int zmk_widget_layer_status_init(struct zmk_widget_layer_status *widget, lv_obj_t *parent) {
     widget->obj = lv_label_create(parent, NULL);
 
     lv_obj_set_size(widget->obj, 64, 10);
 
-    set_layer_symbol(widget->obj);
     sys_slist_append(&widgets, &widget->node);
 
+    widget_layer_status_init();
     return 0;
 }
 
 lv_obj_t *zmk_widget_layer_status_obj(struct zmk_widget_layer_status *widget) {
     return widget->obj;
 }
-
-K_WORK_DEFINE(layer_status_update_work, layer_status_update_cb);
-
-int layer_status_listener(const zmk_event_t *eh) {
-    layer_status_get_state();
-
-    k_work_submit_to_queue(zmk_display_work_q(), &layer_status_update_work);
-    return 0;
-}
-
-ZMK_LISTENER(widget_layer_status, layer_status_listener);
-ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
